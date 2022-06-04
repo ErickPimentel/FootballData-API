ee1be23e480bed4f7d7e7639cbd566f35bdf96bf
Erick Spinelli Pimentel <erickspinellipimentel@gmail.com>
2022-06-04 13:42:37 -0300

    fix: server

--- src/server.ts
+++ src/server.ts
@@ -20,13 +20,14 @@ mongoose
 
 /** Only start the server if Mongo Connects */
 const StartServer = () => {
+    /** Log the request */
     router.use((req, res, next) => {
-        /** Log the Request */
-        Logging.info(`Incomming -> Method: [${req.method}] - Url: [${req.url}] - IP: [${req.socket.remoteAddress}]`);
+        /** Log the req */
+        Logging.info(`Incomming - METHOD: [${req.method}] - URL: [${req.url}] - IP: [${req.socket.remoteAddress}]`);
 
-        req.on('finish', () => {
-            /** Log the Response */
-            Logging.info(`Incomming -> Method: [${req.method}] - Url: [${req.url}] - IP: [${req.socket.remoteAddress}] - Status: [${res.statusCode}]`);
+        res.on('finish', () => {
+            /** Log the res */
+            Logging.info(`Result - METHOD: [${req.method}] - URL: [${req.url}] - IP: [${req.socket.remoteAddress}] - STATUS: [${res.statusCode}]`);
         });
 
         next();
@@ -34,32 +35,32 @@ const StartServer = () => {
 
     router.use(express.urlencoded({ extended: true }));
     router.use(express.json());
+
+    /** API Rules */
+    router.use((req, res, next) => {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+
+        if (req.method == 'OPTIONS') {
+            res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+            return res.status(200).json({});
+        }
+
+        next();
+    });
+
+    /** Routes */
+
+    /** Healthcheck */
+    router.get('/ping', (req, res, next) => res.status(200).json({ message: 'pong' }));
+
+    /** Error handling */
+    router.use((req, res, next) => {
+        const error = new Error('not found');
+        Logging.error(error);
+
+        return res.status(404).json({ message: error.message });
+    });
+
+    http.createServer(router).listen(config.server.port, () => Logging.info(`Server is running on port ${config.server.port}`));
 };
-
-/** API Rules */
-router.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-
-    if (req.method == 'OPTIONS') {
-        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-        return res.status(200).json({});
-    }
-
-    next();
-});
-
-/** Routes */
-
-/** Healthcheck */
-router.get('/ping', (req, res, next) => res.status(200).json({ message: 'pong' }));
-
-/** Error handling */
-router.use((req, res, next) => {
-    const error = new Error('not found');
-    Logging.error(error);
-
-    return res.status(404).json({ message: error.message });
-});
-
-http.createServer(router).listen(config.server.port, () => Logging.info(`Server is running on port ${config.server.port}.`));
