fc0af33665481e0f00c52598e4a7bcc65cd29fc6
Erick Spinelli Pimentel <erickspinellipimentel@gmail.com>
2022-06-10 14:04:34 -0300

    refactor: ContractController async functions

--- src/controllers/ContractController.ts
+++ src/controllers/ContractController.ts
@@ -2,44 +2,56 @@ import { NextFunction, Request, Response } from 'express';
 import mongoose from 'mongoose';
 import Contract from '../models/Contract';
 
-const createContract = (req: Request, res: Response, next: NextFunction) => {
+const createContract = async (req: Request, res: Response, next: NextFunction) => {
     const contract = new Contract({
         _id: new mongoose.Types.ObjectId(),
         ...req.body
     });
 
-    return contract
-        .save()
-        .then((contract) => res.status(201).json({ contract }))
-        .catch((error) => res.status(500).json({ error }));
+    try {
+        const contract_1 = await contract.save();
+        return res.status(201).json({ contract });
+    } catch (error) {
+        return res.status(500).json({ error });
+    }
 };
 
-const getContract = (req: Request, res: Response, next: NextFunction) => {
+const getContract = async (req: Request, res: Response, next: NextFunction) => {
     const contractId = req.params.contractId;
 
-    return Contract.findById(contractId)
-        .then((contract) => (contract ? res.status(200).json({ contract }) : res.status(404).json({ message: 'Not found' })))
-        .catch((error) => res.status(500).json(error));
+    try {
+        const contract = await Contract.findById(contractId);
+        return contract ? res.status(200).json({ contract }) : res.status(404).json({ message: 'Not found' });
+    } catch (error) {
+        return res.status(500).json(error);
+    }
 };
 
-const getAll = (req: Request, res: Response, next: NextFunction) => {
-    return Contract.find()
-        .then((contracts) => res.status(200).json({ contracts }))
-        .catch((error) => res.status(500).json({ error }));
+const getAll = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const contracts = await Contract.find();
+        return res.status(200).json({ contracts });
+    } catch (error) {
+        return res.status(500).json({ error });
+    }
 };
 
-const deleteContract = (req: Request, res: Response, next: NextFunction) => {
+const deleteContract = async (req: Request, res: Response, next: NextFunction) => {
     const contractId = req.params.contractId;
 
-    return Contract.findByIdAndDelete(contractId).then((contract) => (contract ? res.status(201).json({ message: 'deleted' }) : res.status(404).json({ message: 'Not found' })));
+    const contract = await Contract.findByIdAndDelete(contractId);
+    return contract ? res.status(201).json({ message: 'deleted' }) : res.status(404).json({ message: 'Not found' });
 };
 
-const updateContract = (req: Request, res: Response, next: NextFunction) => {
+const updateContract = async (req: Request, res: Response, next: NextFunction) => {
     const contractId = req.params.contractId;
 
-    return Contract.findByIdAndUpdate(contractId, req.body, { new: true })
-        .then((contract) => (contract ? res.status(200).json({ contract }) : res.status(404).json({ message: 'Not found' })))
-        .catch((error) => res.status(500).json(error));
+    try {
+        const contract = await Contract.findByIdAndUpdate(contractId, req.body, { new: true });
+        return contract ? res.status(200).json({ contract }) : res.status(404).json({ message: 'Not found' });
+    } catch (error) {
+        return res.status(500).json(error);
+    }
 };
 
 export default { createContract, getContract, getAll, deleteContract, updateContract };
